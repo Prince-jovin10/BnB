56b5c0bb76b37b325fd04aab93101bd57f83e25a
Vishnu teja <vishnuteja476@gmail.com>
2024-11-16 15:22:17 +0530

    added changes

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,49 +1,59 @@
-import React from 'react'
-import Header from '../bunkandbeyond/components/Header/Header'
-import BannerSection from '../bunkandbeyond/components/BannerSection/BannerSection'
-import AboutUs from '../bunkandbeyond/components/AboutUs/AboutUs'
-import Amenities from '../bunkandbeyond/components/Amenities/Amenities'
-import FeaturedRooms from '../bunkandbeyond/components/FeaturedRooms/FeaturedRooms'
-import UpcomingProjects from '../bunkandbeyond/components/UpcomingProjects/UpcomingProjects'
-import GetInTouch from '../bunkandbeyond/components/GetInTouch/GetInTouch'
-import Location from '../bunkandbeyond/components/Location/Location'
-import Footer from '../bunkandbeyond/components/Footer/Footer'
-import MobileHeader from '../bunkandbeyond/components/MobileHeader/MobileHeader'
-import { useMediaQuery } from 'react-responsive'
-import Style from './Home.module.css'
-import MobileBanner from '../bunkandbeyond/components/MobileBanner/MobileBanner'
+import React from "react";
+import Header from "../bunkandbeyond/components/Header/Header";
+import BannerSection from "../bunkandbeyond/components/BannerSection/BannerSection";
+import AboutUs from "../bunkandbeyond/components/AboutUs/AboutUs";
+import Amenities from "../bunkandbeyond/components/Amenities/Amenities";
+import FeaturedRooms from "../bunkandbeyond/components/FeaturedRooms/FeaturedRooms";
+import UpcomingProjects from "../bunkandbeyond/components/UpcomingProjects/UpcomingProjects";
+import GetInTouch from "../bunkandbeyond/components/GetInTouch/GetInTouch";
+import Location from "../bunkandbeyond/components/Location/Location";
+import Footer from "../bunkandbeyond/components/Footer/Footer";
+import MobileHeader from "../bunkandbeyond/components/MobileHeader/MobileHeader";
+import { useMediaQuery } from "react-responsive";
+import Style from "./Home.module.css";
+import MobileBanner from "../bunkandbeyond/components/MobileBanner/MobileBanner";
 
 import { Element } from "react-scroll";
 
 const Home = () => {
-  const isMobile = useMediaQuery({ query: '(max-width: 800px)' })
+  const isMobile = useMediaQuery({ query: "(max-width: 800px)" });
   return (
     <div className={Style.main}>
-     {isMobile ?<MobileHeader/> :  <Header />}
-      {/* <BannerSection/>
-      <AboutUs/>  */}
-      <MobileBanner/>
-     {/* <Amenities/> 
-     <FeaturedRooms/>
-     </div>
-     <div>
-     <UpcomingProjects/>
-     </div>
+      <div>{isMobile ? <MobileHeader /> : <Header />}</div>
 
-     <Element name='contact'>
-     <div>
-     <GetInTouch/>
-     </div>
-     </Element>
+      <div>{isMobile ? <MobileBanner /> : <BannerSection />}</div>
 
-     <div>
-     <Location/>
-     </div>
-     <div>
-     <Footer/>
-     </div>*/}
-    </div> 
-  )
-}
+      <Element name="about">
+      <div>
+        <AboutUs />
+      </div>
+      </Element>
 
-export default Home
+      <div>
+        <Amenities />
+      </div>
+
+      <div>
+        <FeaturedRooms />
+      </div>
+      <div>
+        <UpcomingProjects />
+      </div>
+
+      <Element name="contact">
+        <div>
+          <GetInTouch />
+        </div>
+      </Element>
+
+      <div>
+        <Location />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/bunkandbeyond/components/UpcomingProjects/UpcomingProjects.jsx
+++ src/bunkandbeyond/components/UpcomingProjects/UpcomingProjects.jsx
@@ -8,6 +8,8 @@ import Pune from "../../assets/upcomingprojects/pune.jpg";
 
 import { useRef } from "react";
 
+import { useMediaQuery } from "react-responsive";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import {
@@ -19,10 +21,11 @@ import {
 
 const UpcomingProjects = () => {
 
-  
-  const containerRef = useRef(null)
+  const isMobile = useMediaQuery({ query: "(max-width: 600px)" });
 
-  const scrollAmount = 60;
+  const containerRef = useRef(null);
+
+  const scrollAmount = 120;
 
   const handleNext = () => {
     if (containerRef.current) {
@@ -52,52 +55,41 @@ const UpcomingProjects = () => {
     }
   };
 
-
   return (
     <>
-     <h2 className={styles.heading}>Coming Soon</h2>
-<<<<<<< HEAD
-    <div className={styles.container} ref={containerRef}>
-     
-      <div className={styles.projects} >
-=======
-    <div className={styles.container}>
-     
-      <div className={styles.projects}>
->>>>>>> 4acf6a7e81066e59f7c41c9b6e7f50ee6c38f3bb
-        <div className={styles.project}>
-          <img src={Delhi} alt="Delhi" className={styles.image} />
-          <p className={styles.name}>Delhi</p>
+      <h2 className={styles.heading}>Coming Soon</h2>
+      <div className={styles.container} ref={containerRef}>
+        <div className={styles.projects}>
+          <div className={styles.project}>
+            <img src={Delhi} alt="Delhi" className={styles.image} />
+            <p className={styles.name}>Delhi</p>
+          </div>
+          <div className={styles.project}>
+            <img src={Hyderabad} alt="Hyderabad" className={styles.image} />
+            <p className={styles.name}>Hyderabad</p>
+          </div>
+          <div className={styles.project}>
+            <img src={Bangalore} alt="Bengaluru" className={styles.image} />
+            <p className={styles.name}>Bengaluru</p>
+          </div>
+          <div className={styles.project}>
+            <img src={Mumbai} alt="Bengaluru" className={styles.image} />
+            <p className={styles.name}>Mumbai</p>
+          </div>
+          <div className={styles.project}>
+            <img src={Pune} alt="Bengaluru" className={styles.image} />
+            <p className={styles.name}>Pune</p>
+          </div>
         </div>
-        <div className={styles.project}>
-          <img src={Hyderabad} alt="Hyderabad" className={styles.image} />
-          <p className={styles.name}>Hyderabad</p>
-        </div>
-        <div className={styles.project}>
-          <img src={Bangalore} alt="Bengaluru" className={styles.image} />
-          <p className={styles.name}>Bengaluru</p>
-        </div>
-        <div className={styles.project}>
-          <img src={Mumbai} alt="Bengaluru" className={styles.image} />
-          <p className={styles.name}>Mumbai</p>
-        </div>
-        <div className={styles.project}>
-          <img src={Pune} alt="Bengaluru" className={styles.image} />
-          <p className={styles.name}>Pune</p>
-        </div>
-      </div>
-<<<<<<< HEAD
 
-     
-
-      {/* <div className={styles.arrow}>
+        {/* <div className={styles.arrow}>
         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M14.3 8.3L10.3 12.3L10.3 4.3" stroke="#626262" strokeWidth="2"/>
         </svg>
       </div> */}
-    </div>
-    <div className={styles.arrowContainer}>
-        
+      </div>
+
+     {isMobile ? "" :<div className={styles.arrowContainer}>
         <div className={styles.leftArrowContainer} onClick={handlePrev}>
           <FontAwesomeIcon
             icon={faArrowLeft}
@@ -111,16 +103,8 @@ const UpcomingProjects = () => {
             className={styles.arrowIconRight}
           />
         </div>
+      </div> } 
 
-=======
-      
-    </div>
-    <div className={styles.arrow}>
-        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M14.3 8.3L10.3 12.3L10.3 4.3" stroke="#626262" strokeWidth="2"/>
-        </svg>
->>>>>>> 4acf6a7e81066e59f7c41c9b6e7f50ee6c38f3bb
-      </div>
     </>
   );
 };
